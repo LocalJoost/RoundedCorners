cd6058fe4ad4b7afcf0eff485a8ccd764e8fe608
Joost van Schaik <joostvanschaik@outlook.com>
2021-07-28 15:44:16 +0200

    Code checkin

---
+++ Assets/Application/Scripts/MaterialAdapter.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class MaterialAdapter : MonoBehaviour
+{
+    [SerializeField]
+    private List<Material> replacementMaterials = new List<Material>();
+
+    [SerializeField]
+    private string replacementPrefix = "Round";
+    
+    void Start()
+    {
+        var replacementMaterialsDictionary = 
+            replacementMaterials.ToDictionary(material => 
+                material.name.Replace(replacementPrefix, string.Empty));
+
+        var renderers = gameObject.GetComponentsInChildren<MeshRenderer>(true);
+        foreach (var r in renderers)
+        {
+            if (r.sharedMaterial == null) continue;
+            if (replacementMaterialsDictionary.ContainsKey(r.sharedMaterial.name))
+            {
+                r.sharedMaterial = replacementMaterialsDictionary[r.sharedMaterial.name];
+            }
+        }
+    }
+}
